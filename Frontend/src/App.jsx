2140554a575f36eab20a636885cb9f1bebd29b1f
Sachin Saroj <sachin.saroj.ug23@nsut.ac.in>
2026-08-14 00:46:31 +0530

    Fixed API endpoint URL

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post('https://ai-code-reviewer-s3ww.onrender.com/', { code })
+    const response = await axios.post('https://ai-code-reviewer-s3ww.onrender.com/ai/get-review', { code })
     setReview(response.data)
   }
 
