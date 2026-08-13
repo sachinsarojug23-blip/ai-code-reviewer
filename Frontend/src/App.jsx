0d079cc60a333cf75ffbb3f0614f90591a3a46ea
Sachin Saroj <sachin.saroj.ug23@nsut.ac.in>
2026-08-14 00:20:09 +0530

    Updated backend production URL

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    const response = await axios.post('https://ai-code-reviewer-s3ww.onrender.com/', { code })
     setReview(response.data)
   }
 
